2b0c383a9cdc1051f085abf83929e67d9764399a
Gonzalez Alvarez Maria Fernanda <99632505+mierri@users.noreply.github.com>
2024-11-02 04:37:25 -0600

    Avances formulario evaluacion

--- src/home/pages/FormularioEvaluacion.jsx
+++ src/home/pages/FormularioEvaluacion.jsx
@@ -2,8 +2,12 @@
 
 export const FormularioEvaluacion = () => {
   return (
-    <main className="min-h-screen">
-        {/* Aqui va su html */}
+    <main className="min-h-screen ">
+      <>
+        <div className="form-container">
+          <h1 className="title-form-page">Prueba</h1>
+        </div>
+      </>
     </main>
   )
 }
