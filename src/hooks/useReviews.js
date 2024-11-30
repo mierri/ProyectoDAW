58d20a45833bde8fceb433d54a7b9b67e989ea1a
Osmar <osmarcc70@gmail.com>
2024-11-29 20:11:05 -0600

    efectos secundarios

--- src/hooks/useReviews.js
+++ src/hooks/useReviews.js
@@ -1,13 +1,16 @@
 import reviewsApi from "../api/reviewsApi";
+import { useSearches } from "./useSearches";
 
 export const useReviews = () => {
+    const {startSearchAllMaestros, startSearchAllMaestrosPorFacultades} = useSearches();
     const urlBase = '/review';
 
     const startNewReview = async (newReview) => {
       try {
         const { data } = await reviewsApi.post(`${urlBase}/new`, newReview);
+        startSearchAllMaestros();
+        startSearchAllMaestrosPorFacultades();
         return data.msg;
-
       } catch (error) {
         if (error.response) {
           throw new Error(error.response.data.msg);
