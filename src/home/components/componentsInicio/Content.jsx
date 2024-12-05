1aa598a5cdb5356cf3466adfa38acdb31af4586d
Osmar <osmarcc70@gmail.com>
2024-12-05 13:32:31 -0600

    terminado 1.0

--- src/home/components/componentsInicio/Content.jsx
+++ src/home/components/componentsInicio/Content.jsx
@@ -4,14 +4,22 @@ export const Content = () => {
   return (
     <div className="content">
       <div className="purpose">
-        <h2><span className="yellow">Nuestro</span> <span className="blue">Propósito</span></h2>
-        <p>En cada período académico, los estudiantes enfrentan el reto de seleccionar cursos y profesores sin suficiente información. Nuestra plataforma centraliza las evaluaciones y opiniones de los estudiantes, ayudándote a tomar decisiones más informadas y ajustadas a tus intereses y estilo de aprendizaje.</p>
+        <h2>
+          <span className="yellow"><b>Nuestro</b></span> <span className="blue"><b>Propósito</b></span>
+        </h2>
+        <p>
+          En cada período académico, los estudiantes enfrentan el reto de seleccionar cursos y profesores sin suficiente información. Nuestra plataforma centraliza las evaluaciones y opiniones de los estudiantes, ayudándote a tomar decisiones más informadas y ajustadas a tus intereses y estilo de aprendizaje.
+        </p>
       </div>
       <div className="latest-evaluations">
-        <h2><span className="blue">Últimas</span> <span className="yellow">Evaluaciones</span></h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, ridiculus sociosqu nec a mi maecenas dignissim ut ante purus pulvinar libero habitasse consequat.</p>
+        <h2>
+          <span className="blue"><b>Últimas</b></span> <span className="yellow"><b>Evaluaciones</b></span>
+        </h2>
+        <p>
+          Consulta las opiniones más recientes de tus compañeros sobre cursos y profesores. Conoce sus experiencias y aprovecha esta información para tomar decisiones informadas que se ajusten a tus metas académicas.
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
--- src/home/components/componentsInicio/ImageSection.jsx
+++ src/home/components/componentsInicio/ImageSection.jsx
@@ -1,14 +1,22 @@
+import { Link } from 'react-router-dom';
 import './ImageSection.css';
 
 export const ImageSection = () => {
   return (
     <div className="image-section">
-      <img src="/students_group.jpg" alt="Estudiantes" />
+      <div className="image-container">
+        <img src="/students_group.jpg" alt="Estudiantes" />
+        <img src="/uady_image.jpg" alt="UADY" />
+      </div>
       <div className="buttons">
-        <button className="btn-primary">Regístrate y comienza a evaluar</button>
-        <button className="btn-primary">Evaluaciones más recientes</button>
+        <Link to={'/auth'}>
+          <button className="btn-primary">Regístrate y comienza a evaluar</button>
+        </Link>
+        <Link to={'/pages/evaluaciones'}>
+          <button className="btn-primary">Evaluaciones más recientes</button>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
